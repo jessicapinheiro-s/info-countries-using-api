a631d4a29385645e6412eba4269a6b6412232939
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2024-04-02 11:36:36 -0300

    ajustando espaçamento, inserindo header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MeuComponente from './components/countries';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <MeuComponente/>
     </div>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import { HeaderElement } from "./header-style"
+
+const Header = () => {
+    return (
+        <HeaderElement>
+            <h1>Countries</h1>
+            <p>Below is a list of all countries along with the respective detailed information for each one</p>
+        </HeaderElement>
+        
+    )
+}
+export default Header;
